02f7ba83840f5b70cf620623fc08317037ded46e
rennaiqian <rennaiqian@126.com>
2018-05-12 20:52:56 +0800

    React router version 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './Routes'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Routes/>, document.getElementById('root'))
+registerServiceWorker()
