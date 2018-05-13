21fd4f5291d1df996f6eb5331fca87ceb54064d8
rennaiqian <rennaiqian@126.com>
2018-05-13 23:32:29 +0800

    Use import function

--- src/Routes.js
+++ src/Routes.js
@@ -1,68 +1,58 @@
-import React from 'react';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-import {Provider} from 'react-redux';
-import {combineReducers} from 'redux';
+import React from 'react'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import { combineReducers } from 'redux'
 
-import {syncHistoryWithStore} from 'react-router-redux';
+import { syncHistoryWithStore } from 'react-router-redux'
 
-import App from './pages/App.js';
-import store from './Store.js';
+import App from './pages/App.js'
+import store from './Store.js'
 
 const createElement = (Component, props) => {
   return (
-    <Provider store={store}>
-      <Component {...props} />
+    <Provider store={ store }>
+      <Component { ...props } />
     </Provider>
-  );
-};
+  )
+}
 
-const getHomePage = (nextState, callback) => {
-  require.ensure([], function(require) {
-    callback(null, require('./pages/Home.js').default);
-  }, 'home');
-};
+const getHomePage = async (nextState, callback) => callback(
+  null,
+  (await import(/* webpackChunkName: "Home" */'./pages/Home.js')).default,
+)
 
-const getAboutPage = (nextState, callback) => {
-  require.ensure([], function(require) {
-    callback(null, require('./pages/About.js').default);
-  }, 'about');
-};
+const getAboutPage = async (nextState, callback) => callback(
+  null,
+  (await import(/* webpackChunkName: "About" */'./pages/About.js')).default,
+)
 
-const getCounterPage = (nextState, callback) => {
-  require.ensure([], function(require) {
-    const {page, reducer, stateKey, initialState} = require('./pages/CounterPage.js');
+const getCounterPage = async (nextState, callback) => {
+  const { page, reducer, stateKey, initialState } = await import(/* webpackChunkName: "CounterPage" */'./pages/CounterPage.js')
+  const state = store.getState()
+  store.reset(combineReducers({
+    ...store._reducers,
+    counter: reducer,
+  }), {
+    ...state,
+    [ stateKey ]: initialState,
+  })
+  callback(null, page)
+}
 
-    const state = store.getState();
-    store.reset(combineReducers({
-      ...store._reducers,
-      counter: reducer
-    }), {
-      ...state,
-      [stateKey]: initialState
-    });
+const getNotFoundPage = async (nextState, callback) => callback(null, (await ('./pages/NotFound')).default)
 
-    callback(null, page);
-  }, 'counter');
-};
-
-const getNotFoundPage = (nextState, callback) => {
-  require.ensure([], function(require) {
-    callback(null, require('./pages/NotFound.js').default);
-  }, '404');
-};
-
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(browserHistory, store)
 
 const Routes = () => (
-  <Router history={history} createElement={createElement}>
-    <Route path="/" component={App}>
-      <IndexRoute getComponent={getHomePage} />
-      <Route path="home" getComponent={getHomePage} />
-      <Route path="counter" getComponent={getCounterPage} />
-      <Route path="about" getComponent={getAboutPage} />
-      <Route path="*" getComponent={getNotFoundPage} />
+  <Router history={ history } createElement={ createElement }>
+    <Route path="/" component={ App }>
+      <IndexRoute getComponent={ getHomePage }/>
+      <Route path="home" getComponent={ getHomePage }/>
+      <Route path="counter" getComponent={ getCounterPage }/>
+      <Route path="about" getComponent={ getAboutPage }/>
+      <Route path="*" getComponent={ getNotFoundPage }/>
     </Route>
   </Router>
-);
+)
 
-export default Routes;
+export default Routes
