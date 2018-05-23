59f26342e22a24e38d78f6041465d23911b21a87
rennaiqian <rennaiqian@126.com>
2018-05-23 17:54:58 +0800

    Change base url

--- src/service/config.js
+++ src/service/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://nework-manager.rdc.waibaodashi.com/'
+export const baseUrl = 'http://nework-web.rdc.waibaodashi.com'
 // export const baseUrl = 'http://10.232.43.232:8080'
