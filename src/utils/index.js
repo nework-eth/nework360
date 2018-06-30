23c7ab67b0c71a393f95b15791731a2a5965280c
rennaiqian <rennaiqian@126.com>
2018-07-01 01:02:38 +0800

    Add complaint modal and rate modal

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+export const getRelativeTime = (createTime) => {
+  const fromTimeStamp = Date.parse(createTime)
+  const now = Date.now()
+  const relativeTime = now - fromTimeStamp
+  const month = Math.floor(relativeTime / (24 * 3600 * 30 * 1000))
+  if (month < 1) {
+    return `${Math.floor(relativeTime / (24 * 3600 * 1000))}天`
+  }
+  if (month < 12) {
+    return `${month}个月`
+  }
+  if (month > 12) {
+    const year = Math.floor(month / 12)
+    const extraMonth = month % 12
+    if (extraMonth) {
+      return `${year}年${extraMonth}个月`
+    }
+    return `${year}年`
+  }
+}
+
