d313a546110fe043ddf91a3762e9e93e5a2aacb0
rennaiqian <rennaiqian@126.com>
2018-07-12 09:31:05 +0800

    Add match result default avatar

--- src/pages/PostDemand/MatchResultCardItem.js
+++ src/pages/PostDemand/MatchResultCardItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MatchResultCardItem ({nickname, avatarSrc, handleClick}) {
   return <div className="match-result-card-item" onClick={ handleClick }>
     <img
-      src={ avatarSrc }
+      src={ avatarSrc || './images/headshot-default.png' }
       alt="头像"
       width="50px"
       height="50px"
