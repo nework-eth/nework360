9759ca5f08fdd54dc15e9ee925683ac892eb7da9
rennaiqian <rennaiqian@126.com>
2018-07-19 10:25:08 +0800

    Fix pay result

--- src/pages/WechatPay/WechatPay.js
+++ src/pages/WechatPay/WechatPay.js
@@ -1,11 +1,15 @@
 import { Button, message } from 'antd'
 import QRCode from 'qrcode.react'
 import React, { Component } from 'react'
-import { browserHistory } from 'react-router'
+import { browserHistory, Link } from 'react-router'
 import { getPayResult } from '../../service/pay/pay'
 import './static/style/index.less'
 
 class WechatPay extends Component {
+  state = {
+    result: '',
+  }
+
   polling = () => {
     let timer = setInterval(async () => {
       try {
@@ -18,16 +22,18 @@ class WechatPay extends Component {
           if (status === 'succ') {
             message.success('购买成功')
             clearInterval(timer)
-            if (this.props.location.state.type === 'cule') {
-              browserHistory.push('/wallet')
-            } else {
-              browserHistory.push('/list')
-            }
+            this.setState({
+              result: 'succ',
+            })
             return
           }
           if (status === 'fail') {
             message.error('购买失败')
             clearInterval(timer)
+            this.setState({
+              result: 'fail',
+            })
+
           }
         }
       } catch (e) {
@@ -41,8 +47,33 @@ class WechatPay extends Component {
   handleGoBack = () => {
     browserHistory.go(-1)
   }
+  continueToPay = () => {
+    this.setState({
+      result: '',
+    })
+    this.polling()
+  }
 
   render () {
+    if (this.state.result === 'succ') {
+      return (
+        <div className="wechat-pay-result">
+          <img src="/images/pay-succeed.png" alt="支付成功" width='50' height='50'/>
+          <h2>支付成功</h2>
+          <p>返回 <Link to='/list'>我的订单</Link></p>
+        </div>
+      )
+    }
+    if (this.state.result === 'fail') {
+      return (
+        <div className="wechat-pay-result">
+          <img src="/images/pay-error.png" alt="支付失败" width='50' height='50'/>
+          <h2>支付成功</h2>
+          <p>返回 <Link to='/list'>我的订单</Link> 或 <span className="virtual-link" onClick={ this.continueToPay }>继续支付</span>
+          </p>
+        </div>
+      )
+    }
     return (
       <div className="wechat-pay-container">
         <h2>微信扫码支付</h2>
