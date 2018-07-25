293100dd4386deb220c1caa13a8758617370b872
rennaiqian [任乃千] <rennaiqian@126.com>
2018-07-25 10:11:30 +0800

    Fix config

--- src/service/config.js
+++ src/service/config.js
@@ -1,6 +1,8 @@
 import { message } from 'antd'
 import axios from 'axios'
 import { browserHistory } from 'react-router'
+import * as ActionTypes from './components/NavMenu/actionTypes'
+import store from './Store'
 
 axios.defaults.baseURL = 'http://nework-web.rdc.waibaodashi.com'
 
@@ -10,6 +12,8 @@ axios.interceptors.response.use(res => {
   const {data: {code, desc}} = res
   if (code === 501) {
     browserHistory.push('/login')
+    store.dispatch({type: ActionTypes.SETUSER, user: {}})
+    store.dispatch({type: ActionTypes.SETUSERID, userId: undefined})
     return res
   }
   if (code !== 200 && code !== 302) {
