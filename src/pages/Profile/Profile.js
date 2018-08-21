e4c0652d16fa726f38070bed8d0242579d89d3d5
rennaiqian [任乃千] <rennaiqian@126.com>
2018-08-21 17:09:55 +0800

    Fix style

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -53,16 +53,16 @@ const logoSrcList = [
 
 const SkillItem = ({logoSrc, title, index}) => (<div className="skill-card">
   <img
-    src={ logoSrc }
+    src={logoSrc}
     alt="icon"
     width="40"
     height="40"
   />
   <p className="title">
-    { title }
+    {title}
   </p>
   <p className="skill-index">
-    技能{ index + 1 }
+    技能{index + 1}
   </p>
 </div>)
 
@@ -154,75 +154,76 @@ class Profile extends Component {
     return (
       <div className="profile-container">
         <main>
-          <img src={ avatar || './images/headshot-default.png' } alt="头像" width="100" height="100" className="avatar"/>
-          <h2>您好，我是{ nickname }</h2>
+          <img src={avatar || './images/headshot-default.png'} alt="头像" width="100" height="100" className="avatar"/>
+          <h2>您好，我是{nickname}</h2>
           <div className="info">
-            <p className="position">{ isPartyB ? city : this.props.location.cityName } · { country }</p>
+            <p className="position">{isPartyB ? city : this.props.location.cityName} · {country}</p>
             <Rate
               allowHalf
               disabled
-              value={ getRate(ave) }
-              character={ <i className="iconfont icon-rate-star-full"/> }
+              value={getRate(ave)}
+              character={<i className="iconfont icon-rate-star-full"/>}
             />
-            <p className="rate">{ ave }</p>
-            <p className="evaluation">({ count }条评价)</p>
+            <p className="rate">{ave}</p>
+            <p className="evaluation">({count}条评价)</p>
           </div>
-          { description && <p className="introduce">
-            { description }
-          </p> }
+          {description && <p className="introduce">
+            {description}
+          </p>}
           <div className="information-container">
-            <div><i className="iconfont icon-hire"/>已雇佣{ bossTimes }次</div>
-            { isPartyB && <div><i className="iconfont icon-hire"/>被雇佣{ hireTimes }次</div> }
-            { checkStatus === 2 && <div><i className="iconfont icon-identified"/>实名认证</div> }
-            <div><i className="iconfont icon-joined-time"/>已加入{ getRelativeTime(createTime) }</div>
-            { phoneNumber && <div><i className="iconfont icon-identified-phone"/>手机认证</div> }
-            { email && <div><i className="iconfont icon-nav-message"/>邮箱认证</div> }
+            <div><i className="iconfont icon-hire"/>已雇佣{bossTimes}次</div>
+            {isPartyB && <div><i className="iconfont icon-hire"/>被雇佣{hireTimes}次</div>}
+            {checkStatus === 2 && <div><i className="iconfont icon-identified"/>实名认证</div>}
+            <div><i className="iconfont icon-joined-time"/>已加入{getRelativeTime(createTime)}</div>
+            {phoneNumber && <div><i className="iconfont icon-identified-phone"/>手机认证</div>}
+            {email && <div><i className="iconfont icon-nav-message"/>邮箱认证</div>}
           </div>
-          { isPartyB &&
+          {isPartyB &&
           <div className="skill-carousel-container"><h3
-            className="skill-title">技能<span>（{ secondarySkillList.length }）</span>
+            className="skill-title">技能<span>（{secondarySkillList.length}）</span>
           </h3>
             <Carousel
-              dots={ false }
-              infinite={ false }
-              slidesToShow={ 4 }
-              slidesToScroll={ 1 }
-              arrows={ true }
+              dots={false}
+              infinite={false}
+              slidesToShow={4}
+              slidesToScroll={1}
+              arrows={true}
             >
               {
                 secondarySkillList.map(({secondServiceTypeName, firstServiceTypeName}, index) => <SkillItem
-                  logoSrc={ logoSrcList.find(src => src.includes(firstServiceTypeName)) || './images/其他-icon.png' }
-                  title={ secondServiceTypeName }
-                  index={ index }
-                  key={ index }
+                  logoSrc={logoSrcList.find(src => src.includes(firstServiceTypeName)) || './images/其他-icon.png'}
+                  title={secondServiceTypeName}
+                  index={index}
+                  key={index}
                 />)
               }
             </Carousel>
-          </div> }
-          { isPartyB && <div>
+          </div>}
+          {isPartyB && <div>
             <h3 className="location">营业时间与地点</h3>
             <div className="time-location-container">
               <DayPickerRangeController
-                disabled={ true }
-                numberOfMonths={ 1 }
+                style={{width: '340px'}}
+                disabled={true}
+                numberOfMonths={1}
                 hideKeyboardShortcutsPanel
                 monthFormat="YYYY[年]M[月]"
-                isDayHighlighted={ isDayHighlighted(serviceTime.split(',')) }
-                isDayBlocked={ (value) => !moment().isBefore(value) }
+                isDayHighlighted={isDayHighlighted(serviceTime.split(','))}
+                isDayBlocked={(value) => !moment().isBefore(value)}
               />
               <div id="mapContainer"/>
             </div>
-          </div> }
+          </div>}
 
         </main>
         <footer>
           {
             this.state.data.isPartyB
               ? this.props.user.userId === this.state.data.userId
-              ? <Button type="primary" onClick={ this.jumpToEditData }>编辑</Button>
-              : <Button type="primary" onClick={ this.jumpToPostDemand }>联系</Button>
+              ? <Button type="primary" onClick={this.jumpToEditData}>编辑</Button>
+              : <Button type="primary" onClick={this.jumpToPostDemand}>联系</Button>
               : this.props.user.userId === this.state.data.userId
-              ? <Button type="primary" onClick={ this.jumpToEditData }>编辑</Button>
+              ? <Button type="primary" onClick={this.jumpToEditData}>编辑</Button>
               : ''
           }
         </footer>
